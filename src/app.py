cf03862fb2adcba3c8a554f14c63e18f2fbc37d2
P-cm-source <56630358+P-cm-source@users.noreply.github.com>
2021-09-01 00:06:54 +0530

    changes in app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1> hello world</h1>"
+    return "<h1> hello Pranav how are you</h1>"
 
 if __name__ == "__main__" :
     app.run()
